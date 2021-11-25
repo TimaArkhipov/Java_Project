9014a4a306514693d26509292e6eac136c29fd02
Bolamir <78905392+Bolamir@users.noreply.github.com>
2021-11-25 10:49:08 +0300

    Add files via upload

--- Database.java
+++ Database.java
@@ -1,3 +1,5 @@
+package com.example.myapplication;
+
 import java.io.Externalizable;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,23 +15,25 @@ import java.util.List;
 import java.util.Properties;
 
 public class Database implements Externalizable {
-	private List <Deal> deals=new  ArrayList<>(); 
+	private List <Deal> deals=new  ArrayList<>();
 	public static String fileName;
 	
 	
-	public static final String PATH_TO_PROPERTIES ="C:\\Users\\Vova\\OneDrive\\Desktop\\Lab_Java\\src\\Prop.properties";
+	public static final String PATH_TO_PROPERTIES ="/Prop.properties";
+
 	Database(){}
+
 	Database(List<Deal> deals){
 		this.deals=deals;
 	}
 	public static void PreLoad()
 	{		 
 	        FileInputStream fileInputStream;
-	        //инициализируем специальный объект Properties
-	        //типа Hashtable для удобной работы с данными
+	        //�������������� ����������� ������ Properties
+	        //���� Hashtable ��� ������� ������ � �������
 	        Properties prop = new Properties();
 	        try {
-	            //обращаемся к файлу и получаем данные
+	            //���������� � ����� � �������� ������
 	            fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
 	            prop.load(fileInputStream);
 	 
@@ -42,7 +46,11 @@ public class Database implements Externalizable {
 	        }
 	 
 	}
-	
+
+	public List<Deal> getDeals() {
+		return deals;
+	}
+
 	public void save() throws IOException
 	{
 		
@@ -59,12 +67,13 @@ public class Database implements Externalizable {
 	
 	public void load() throws Exception 
 	{
-		FileInputStream fileInputStream = null;
-		ObjectInputStream objectInputStream = null;
+		FileInputStream fileInputStream=null;
+		ObjectInputStream objectInputStream=null;
 		try {
 		fileInputStream = new FileInputStream(fileName);
 		objectInputStream = new ObjectInputStream(fileInputStream);
-	    	this.deals=(List<Deal>) objectInputStream.readObject();
+	    this.deals=(List<Deal>) objectInputStream.readObject();
+	    
 	    
 		}
 		catch(Exception e)
@@ -78,63 +87,71 @@ public class Database implements Externalizable {
 		
 	}
 	@Override
-	public void writeExternal(ObjectOutput newOutput) throws IOException {
+	public void writeExternal(ObjectOutput out) throws IOException {
 		out.writeObject(this.deals.size());
 		for(Deal i : deals)
 		{
-			i.writeExternal(newOutput);
+			i.writeExternal(out);
 		}
 		
 	}
 	@Override
-	public void readExternal(ObjectInput newInput) throws IOException, ClassNotFoundException {
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
 		int count=(int)in.readObject();
-		for(int i=0; i < count; i++)
+		System.out.println("Good6");
+		for(int i=0; i<count;i++)
 		{
 			Deal m = new Deal();
-			m.readExternal(newInput);
+			m.readExternal(in);
 			deals.add(m);
+			System.out.println("Goodit");
 		}
 		
 	}
 	@Override
 	public String toString() {
-		String s = new String() ;
+		String s = "";
 		for(Deal i : deals)
 		{
-			s += i.toString() + '\n';
+			s=s+i.toString()+'\n';
 		}
+		
 		return s;
 	}
 	
-	public static void main(String[] args) //временное
+	public static void main(String[] args)
 	{
 		Database.PreLoad();
+		
+		
 		List<Deal> b=new ArrayList<>();
-		for(int j = 0; j < 3; j++)
+		
+		for(int j=0;j<3;j++)
 		{
-			HashSet<TaskReport> tr = new HashSet<>();
-			for(int i = 0; i < 4; i++)
-			{
-				TaskReport e = new TaskReport(new Date(0),new Date(20));
-				tr.add(e);
-			}
-			Deal thing=new Deal("Death", 1931707, "Smert-smert-smert", tr);
-			b.add(thing);
+		HashSet<TaskReport> tr = new HashSet<>();
+		for(int i=0; i<4;i++)
+		{
+			TaskReport e = new TaskReport(new Date(0),new Date(20));
+			tr.add(e);
 		}
+		Deal thing=new Deal("Death",1931707,"Smert-smert-smert", tr);
+		b.add(thing);
+		}
+	
 		Database a=new Database(b);
-		try 
-		{
-			a.save();
+		
+		try {
+		a.save();
 		}
-		catch(IOException e) {
+		catch(IOException e)
+		{
 			System.out.println(e.getMessage());
 			System.exit(0);
 		}
+		
 		Database d=new Database();
-		try 
-		{
-			d.load();
+		try {
+		d.load();
 		}
 		catch(Exception e)
 		{
