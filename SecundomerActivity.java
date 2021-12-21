d61301dd94e9a6bf3a6f6b4c0d4697cfec7a56d9
Bolamir <78905392+Bolamir@users.noreply.github.com>
2021-12-22 01:35:29 +0300

    Pause button (timer\secundomer) +stop button timer

--- SecundomerActivity.java
+++ SecundomerActivity.java
@@ -1,13 +1,10 @@
 package com.example.timetracker;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.ContextMenu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,27 +12,31 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.example.timetracker.core.Deal;
+import com.example.timetracker.core.TaskReport;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+
+
 public class SecundomerActivity extends AppCompatActivity {
 
     Spinner spinner;
     private Button newDealButtonS;
-    boolean f = true;
+    private Button pause;
+    boolean begin_end = true;
+    boolean stop_start = true;
     int sec=0;
-  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundomer);
 
         List<Deal> dealList = Arrays.asList(
-                new Deal("Program job", "I make program"),
-                new Deal("Walk", "I walk with my dog"),
+                new Deal("Программирование", "I make program"),
+                new Deal("Прогулка", "I walk with my dog"),
                 new Deal("Workout", "I train at the gym"));
         List<String> nameDealList = new ArrayList<>();
         //deals = (TextView) findViewById(R.id.deal);
@@ -47,11 +48,12 @@ public class SecundomerActivity extends AppCompatActivity {
             nameDealList.add(thing);
         }
 
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, nameDealList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         spinner = (Spinner) findViewById(R.id.spinnerS);
         spinner.setAdapter(adapter);
+
 
         TextView tim=(TextView) findViewById(R.id.textNameSecundomer);
         tim.setOnClickListener(new View.OnClickListener() {
@@ -63,19 +65,20 @@ public class SecundomerActivity extends AppCompatActivity {
             }
         });
         TaskReport taskReport = new TaskReport();
+
         Button startSec=(Button) findViewById(R.id.start);
         startSec.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if (f)
+                if (begin_end)
                 {
-                    f = false;
+                    begin_end = false;
                     startSec.setText( "Стоп" );
                     Date db=new Date();
                     taskReport.setDateStart(db);
                     Handler handler=new Handler();
-                    spinner.setClickable(f);
-                    newDealButtonS.setClickable(f);
+                    spinner.setClickable(begin_end);
+                    newDealButtonS.setClickable(begin_end);
 
                     TextView timeText = (TextView) findViewById(R.id.textTextS);
                     handler.post(new Runnable() {
@@ -86,38 +89,19 @@ public class SecundomerActivity extends AppCompatActivity {
                             int s=sec%60;
                             String time= String.format("%02d:%02d:%02d",h,m,s);
                             timeText.setText(time);
-                            sec=sec+1;
+                            if (stop_start)
+                                sec=sec+1;
                             handler.postDelayed(this,1000);
                         }
                     });
-
-                    Date db=new Date();
-                    taskReport.setDateStart(db);
-                    Handler handler=new Handler();
-
-                    TextView timeText = (TextView) findViewById(R.id.textTextS);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            int h=sec/3600;
-                            int m=sec%3600/60;
-                            int s=sec%60;
-                            String time= String.format("%d:%02d:%02d",h,m,s);
-                            timeText.setText(time);
-                            sec=sec+1;
-                            handler.postDelayed(this,1000);
-                        }
-                    });
-
 
                 }
                 else
                 {
 
-                    f = true;
-
-                    spinner.setClickable(f);
-                    newDealButtonS.setClickable(f);
+                    begin_end = true;
+                    spinner.setClickable(begin_end);
+                    newDealButtonS.setClickable(begin_end);
                     startSec.setText( "Старт" );
                     Intent intent = new Intent(SecundomerActivity.this, ReportActivity.class);
                     Date de=new Date();
@@ -126,7 +110,6 @@ public class SecundomerActivity extends AppCompatActivity {
                     intent.putExtra("TaskReport",taskReport);
                     intent.putExtra("Deal",nameSelectedDeal);
 
-
                     startActivity(intent);
 
                 }
@@ -134,6 +117,7 @@ public class SecundomerActivity extends AppCompatActivity {
 
 
             }
+
         });
 
         newDealButtonS=(Button) findViewById(R.id.newDealButtonS);
@@ -143,6 +127,7 @@ public class SecundomerActivity extends AppCompatActivity {
                 Dialog newDealDialog= new Dialog(SecundomerActivity.this);
                 newDealDialog.setContentView(R.layout.new_deal_layout);
                 newDealDialog.show();
+
                 EditText name=(EditText) newDealDialog.findViewById(R.id.editTextName);
                 EditText description=(EditText) newDealDialog.findViewById(R.id.editTextDescription);
                 Button createNewDeal =(Button) newDealDialog.findViewById(R.id.createDeal);
@@ -157,17 +142,44 @@ public class SecundomerActivity extends AppCompatActivity {
 
 
 
+            }
+        });
+        pause=(Button) findViewById(R.id.pauseButton);
+        pause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (stop_start)
+                {
+                    stop_start = false;
+                    pause.setText( "ПРОДОЛЖИТЬ" );
 
+
+                }
+                else
+                {
+
+                    stop_start = true;
+                    pause.setText( "ПАУЗА" );
+
+                }
             }
         });
 
     }
 
-
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(SecundomerActivity.this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    /*
+    @Override
+
     public boolean onContextItemSelected(@NonNull MenuItem item) {
 
         //deals.setText(item.getTitle().toString() + item.getItemId());
         return super.onContextItemSelected(item);
     }
+    */
 }
